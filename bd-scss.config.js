34d2e99a949eb6e569b87757384b692f940840c8
winterClover <66449394+winterClover@users.noreply.github.com>
2023-12-08 20:39:48 -0600

    Clear all files

--- bd-scss.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('bd-scss/lib/config').Config} */
-export default {
-    meta: {
-        name: "Discord Messenger" /* The official name of your theme. */,
-        scss: "temp" /* The name of your base scss file. */,
-        repo: "vista-discord-theme" /* The name of your theme repository. */,
-        version: "dev" /* The version of your theme. */
-    }
-};
